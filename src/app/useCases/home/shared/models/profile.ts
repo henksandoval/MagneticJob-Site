ef1b25f50b260456032886eb178914df0b3a7e18
AlexandraMarval <alexandrai.marvala@gmail.com>
2024-08-20 12:30:10 +0200

    Add properties by folder

--- src/app/useCases/home/shared/models/profile.ts
+++ src/app/useCases/home/shared/models/profile.ts
@@ -1,8 +1,11 @@
 import { Facts } from "./Facts";
+import { WorkExperience } from "./WorkExperience";
 import { About } from "./about";
 import { Contact } from "./contact";
+import { Education } from "./education";
 import { Resume } from "./resume";
 import { Skills } from "./skills";
+import { Sumary } from "./sumary";
 
 export interface Profile {
 	birthday?: string;
@@ -11,4 +14,7 @@ export interface Profile {
 	facts?: Facts;
 	skills?: Skills;
 	resume?: Resume;
+	workExperience?: WorkExperience;
+	education?: Education;
+	sumary?: Sumary;
 }
