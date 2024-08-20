296a60835c6e82e24e47a246f2943c080d58a11f
AlexandraMarval <alexandrai.marvala@gmail.com>
2024-08-20 12:30:10 +0200

    Add properties by folder

---
+++ src/app/useCases/home/shared/models/education.ts
@@ -0,0 +1,10 @@
+export interface Education {
+  detail: Detail[];
+}
+
+export interface Detail {
+  experience: string;
+  startdate: string;
+  Academy: string;
+  description: string;
+}
---
+++ src/app/useCases/home/shared/models/sumary.ts
@@ -0,0 +1,11 @@
+export interface Sumary {
+  name: string;
+  summary: string;
+  contact: Contact[];
+}
+
+export interface Contact {
+  location: string;
+  phoneNumber: string;
+  aliceBarkle: string;
+}
