c62199bf4ebaad43433a1ef3d89bbaac7a6198e6
AlexandraMarval <alexandrai.marvala@gmail.com>
2024-08-20 13:02:59 +0200

    🐛Create new files by adding the properties in their respective corresponding files

---
+++ src/app/useCases/home/components/profile/resume/resume.component.spec.ts
@@ -0,0 +1,5 @@
+// import { Resume } from "../../../shared/models/resume";
+
+// const mockResume: Resume = {
+
+// }
--- src/app/useCases/home/shared/models/workExperience.ts
+++ src/app/useCases/home/shared/models/workExperience.ts
@@ -1,13 +1,5 @@
+import { Position } from "./position";
+
 export interface WorkExperience {
   position: Position[];
-}
-
-export interface Position {
-  position: string;
-  startdate: string;
-  location: string;
-  communicationDesignLeadership: string;
-  teamLeadership: string;
-  designQualityAssurance: string;
-  expertiseArea: string;
 }
