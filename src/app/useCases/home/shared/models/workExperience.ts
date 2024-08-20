d3c5806eb19d599d04ee21f8c346c9b9f810d36a
Alexandra Marval <alexndra.04@hotmail.com>
2024-08-20 12:30:10 +0200

    Add properties by folder

---
+++ src/app/useCases/home/shared/models/workExperience.ts
@@ -0,0 +1,13 @@
+export interface WorkExperience {
+  position: Position[];
+}
+
+export interface Position {
+  position: string;
+  startdate: string;
+  location: string;
+  communicationDesignLeadership: string;
+  teamLeadership: string;
+  designQualityAssurance: string;
+  expertiseArea: string;
+}
