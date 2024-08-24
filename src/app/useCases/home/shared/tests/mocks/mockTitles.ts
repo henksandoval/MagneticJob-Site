730e05046ff553e7215204fffe918e36a4e129cd
Henk Sandoval <henkalexander.sandoval@external.wolterskluwer.com>
2024-08-24 08:17:32 +0200

    refactor: ♻️ Move all mock models to specified folder by reuse definition

---
+++ src/app/useCases/home/shared/tests/mocks/mockTitles.ts
@@ -0,0 +1,8 @@
+import { Titles } from '../../../shared/models/titles';
+
+export const mockTitles: Titles = {
+  resume: 'Historial test',
+  summary: 'Sumaria',
+  education: 'Educacion',
+  workExperience: 'Experiencia profesional',
+};
