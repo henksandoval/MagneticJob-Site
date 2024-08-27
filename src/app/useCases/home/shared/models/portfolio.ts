570337b1171a183ed73d479b4c4692c2ac0d20d0
AlexandraMarval <alexandrai.marvala@gmail.com>
2024-08-27 11:56:05 +0200

    feat: 🚧 Defined the properties

--- src/app/useCases/home/shared/models/portfolio.ts
+++ src/app/useCases/home/shared/models/portfolio.ts
@@ -1,7 +1,8 @@
-import { Description } from "./description";
-import { Introduction } from "./introduction";
-
-export interface PortFolio{
-    introduction: Introduction[];
-    description: Description[];
+export interface PortFolio {
+  position: string;
+  title: string;
+  description: string;
+  link: string;
+  image: string;
+  type: string;
 }
