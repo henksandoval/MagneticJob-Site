9ff9cbbb14f789d91eb59987c36b889be479c95c
Alexandra Marval <alexndra.04@hotmail.com>
2024-08-28 13:35:56 +0200

    Create mockportfolio for the tests

---
+++ src/app/useCases/home/shared/tests/mocks/mockPortfolio.ts
@@ -0,0 +1,11 @@
+import { PortFolio } from "../../models/portfolio";
+
+export const mockPortfolio: PortFolio = {
+    text: 'Portfolio',
+    position: 1,
+    title: 'Photojam',
+    description: 'Aphasia following other cerebrovascular disease',
+    link: 'http://dummyimage.com/103x100.png/ff4444/ffffff',
+    image: 'http://dummyimage.com/146x100.png/5fa2dd/ffffff',
+    type: 'APP'
+}
