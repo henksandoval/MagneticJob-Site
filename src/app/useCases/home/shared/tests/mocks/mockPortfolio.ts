618ac52217e019e975d4bb7924d247c8c68c80c9
Alexandra marval <Alexandra.marval@eurokaizen.com>
2024-09-03 18:26:19 +0200

    test: 🧪 Add unit test by assert should show the categories

--- src/app/useCases/home/shared/tests/mocks/mockPortfolio.ts
+++ src/app/useCases/home/shared/tests/mocks/mockPortfolio.ts
@@ -6,8 +6,8 @@ export const mockWebPage: WebPage = {
   title: 'Aventuras Animales',
   description: 'Cada fotografía captura momentos únicos y comportamientos fascinantes.',
   link: 'https://www.jetbrains.com/teamcity/?gdmcid=7f94a68d-ad43-4c54-ad20-364c52ae127e',
-  image: 'img/portfolio/portfolio-1.jpg',
-  type: 'APP',
+  image: 'https://images.pexels.com/photos/617278/pexels-photo-617278.jpeg',
+  type: 'CAT',
 };
 
 export const mockWebPage2: WebPage = {
@@ -17,7 +17,7 @@ export const mockWebPage2: WebPage = {
     'Cada imagen captura la esencia de lugares únicos, desde montañas imponentes hasta costas tranquilas, invitándote a explorar la belleza del mundo',
   link: 'https://pixabay.com/es/videos/oceano-playa-ondas-rompiendo-olas-160767/',
   image: 'https://th.bing.com/th/id/OIP.iwFhHHKPOqAJUDO-iSov_wHaE8?rs=1&pid=ImgDetMain',
-  type: 'WEB',
+  type: 'NATURE',
 };
 
 export const mockWebPage3: WebPage = {
@@ -28,10 +28,37 @@ export const mockWebPage3: WebPage = {
   link: 'https://www.istockphoto.com/es/v%C3%ADdeo/reci%C3%A9n-casados-bailando-vals-en-la-pista-de-baile-gm1180011493-330411869',
   image:
     'https://1.bp.blogspot.com/-j-w8B3RW3qI/VwgTezGX_6I/AAAAAAAAFag/-bUAYbTtqzorWspGeEgjbCq41iLIOC9Tw/s1600/Wedding06-main.jpg',
-  type: 'APP',
+  type: 'MARRIES',
+};
+
+export const mockWebPage4: WebPage = {
+  position: 4,
+  title: 'Aventuras Animales',
+  description: 'Cada fotografía captura momentos únicos y comportamientos fascinantes.',
+  link: 'https://www.jetbrains.com/teamcity/?gdmcid=7f94a68d-ad43-4c54-ad20-364c52ae127e',
+  image: 'https://images.pexels.com/photos/617278/pexels-photo-617278.jpeg',
+  type: 'CAT',
+};
+
+export const mockWebPage5: WebPage = {
+  position: 5,
+  title: 'Aventuras Animales',
+  description: 'Cada fotografía captura momentos únicos y comportamientos fascinantes.',
+  link: 'https://www.jetbrains.com/teamcity/?gdmcid=7f94a68d-ad43-4c54-ad20-364c52ae127e',
+  image: 'https://images.pexels.com/photos/617278/pexels-photo-617278.jpeg',
+  type: 'CAT',
+};
+
+export const mockWebPage6: WebPage = {
+  position: 6,
+  title: 'Aventuras Animales',
+  description: 'Cada fotografía captura momentos únicos y comportamientos fascinantes.',
+  link: 'https://www.jetbrains.com/teamcity/?gdmcid=7f94a68d-ad43-4c54-ad20-364c52ae127e',
+  image: 'https://images.pexels.com/photos/617278/pexels-photo-617278.jpeg',
+  type: 'NATURE',
 };
 
 export const mockPortfolio: PortFolio = {
   text: 'Portfolio',
-  webPage: [mockWebPage, mockWebPage2, mockWebPage3],
+  webPage: [mockWebPage, mockWebPage2, mockWebPage3, mockWebPage4, mockWebPage5, mockWebPage6],
 };
