0a8216efd4d9890ac2733eb1f98d860c0c00a9f5
AlexandraMarval <alexandrai.marvala@gmail.com>
2024-09-03 18:26:19 +0200

    test: 🧪 Add unit test by assert should show the categories

--- src/app/useCases/home/components/profile/portfolio/portfolio.component.spec.ts
+++ src/app/useCases/home/components/profile/portfolio/portfolio.component.spec.ts
@@ -27,16 +27,38 @@ describe('PortfolioComponent', () => {
     expect(screen.getByTestId('text')).toHaveTextContent(mockProfile.portfolio.text);
   });
 
+  it('It should show the categories', () => {
+    // const entrada: WebPage[] = [
+    //   {position: 1, description: 'uno', image: 'uno', link: 'uno', title: 'uno', type: 'color' },
+    //   {position: 2, description: 'uno', image: 'uno', link: 'uno', title: 'uno', type: 'web' },
+    //   {position: 3, description: 'uno', image: 'uno', link: 'uno', title: 'uno', type: 'web' },
+    //   {position: 4, description: 'uno', image: 'uno', link: 'uno', title: 'uno', type: 'color' },
+    //   {position: 5, description: 'uno', image: 'uno', link: 'uno', title: 'uno', type: 'color' }
+    //  ];
+
+    //  const salida: string[] = ['color', 'web'];
+
+    const resultadoEsperado: string[] = ['CAT', 'NATURE', 'MARRIES'];
+    //   <ul id="portfolio-flters">
+    //   <li data-testid="all" data-filter="*" class="filter-active">All</li>
+    //   <li data-filter=".filter-app">App</li>
+    //   <li data-filter=".filter-web">Web</li>
+    // </ul>
+    resultadoEsperado.forEach((filter: string) => {
+      expect(screen.getByTestId('filter_' + filter)).toHaveAttribute('data-filter', '.filter-' + filter);
+    });
+  });
+
   it('You must submit all records regarding the portfolio.', () => {
     mockProfile.portfolio.webPage.forEach((webPage: WebPage, index: number) => {
       const id: string = (++index).toString().padStart(2, '0');
       expect(screen.getByTestId('image-src_' + id)).toHaveAttribute('src', webPage.image);
-      expect(screen.getByTestId('position' + id)).toHaveTextContent(webPage.position.toString());
-      expect(screen.getByTestId('title' + id)).toHaveTextContent(webPage.title);
-      expect(screen.getByTestId('type' + id)).toHaveTextContent(webPage.type);
-      expect(screen.getByTestId('description' + id)).toHaveTextContent(webPage.description);
+      expect(screen.getByTestId('position_' + id)).toHaveTextContent(webPage.position.toString());
+      expect(screen.getByTestId('title_' + id)).toHaveTextContent(webPage.title);
+      expect(screen.getByTestId('type_' + id)).toHaveTextContent(webPage.type);
+      expect(screen.getByTestId('description_' + id)).toHaveTextContent(webPage.description);
       expect(screen.getByTestId('image-href' + id)).toHaveAttribute('href', webPage.image);
-      expect(screen.getByTestId('link' + id)).toHaveAttribute('href', webPage.link);
+      expect(screen.getByTestId('link_' + id)).toHaveAttribute('href', webPage.link);
     });
   });
 });
