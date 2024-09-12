024bfbd5c20438d7158c70eeb6b58d971ec91c6c
AlexandraMarval <alexandrai.marvala@gmail.com>
2024-09-12 17:02:02 +0200

    Add method to get the type

--- src/app/useCases/home/components/profile/portfolio/portfolio.component.ts
+++ src/app/useCases/home/components/profile/portfolio/portfolio.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { ProfileService } from '../../../shared/services/profile/profile.service';
 import { AsyncPipe, NgClass, NgFor, NgIf } from '@angular/common';
+import { WebPage } from '../../../shared/models/webPage';
 
 @Component({
   selector: 'app-portfolio',
@@ -12,4 +13,24 @@ import { AsyncPipe, NgClass, NgFor, NgIf } from '@angular/common';
 export class PortfolioComponent {
   private profileService: ProfileService = inject(ProfileService);
   profile$ = this.profileService.profile$;
+  webPage: WebPage[] = [];
+  
+  obtenerLosTipos(): WebPage[] {
+    const unicos: string[] = [];
+    return this.webPage.reduce((reduciendo, valor) => {
+      if (!unicos.includes(valor.type)) {
+        unicos.push(valor.type);
+        reduciendo.push({ type: valor.type,
+          position: 1, 
+          title: '',    
+          description: '', 
+          link: '',     
+          image: '' 
+         });
+      }
+      return reduciendo;
+    }, [] as WebPage[]);
+  }
+
+  // types: string[] = ['CAT', 'NATURE', 'MARRIES'];
 }
