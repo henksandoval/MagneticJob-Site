d4d4283ace1e4785b7b17d24aa113c3725d7ac51
Henk Sandoval <henk.sandoval@outlook.com>
2024-09-13 14:30:49 +0200

    feat: ✨ Add Testimonials interface to template

--- src/app/useCases/home/components/profile/testimonials/testimonials.component.ts
+++ src/app/useCases/home/components/profile/testimonials/testimonials.component.ts
@@ -1,15 +1,41 @@
 import { Component, inject } from '@angular/core';
+import { CarouselModule, OwlOptions } from 'ngx-owl-carousel-o';
 import { ProfileService } from '../../../shared/services/profile/profile.service';
-import { AsyncPipe, NgFor, NgIf } from '@angular/common';
+import { AsyncPipe, JsonPipe, NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-testimonials',
   standalone: true,
-  imports: [NgIf, NgFor, AsyncPipe],
+  imports: [NgIf, NgFor, AsyncPipe, JsonPipe, CarouselModule],
   templateUrl: './testimonials.component.html',
   styles: ``,
 })
 export class TestimonialsComponent {
   private profileService: ProfileService = inject(ProfileService);
   profile$ = this.profileService.profile$;
+
+  customOptions: OwlOptions = {
+    loop: false,
+    mouseDrag: true,
+    touchDrag: false,
+    pullDrag: false,
+    dots: false,
+    navSpeed: 700,
+    navText: ['', ''],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      400: {
+        items: 2,
+      },
+      740: {
+        items: 3,
+      },
+      940: {
+        items: 4,
+      },
+    },
+    nav: true,
+  };
 }
