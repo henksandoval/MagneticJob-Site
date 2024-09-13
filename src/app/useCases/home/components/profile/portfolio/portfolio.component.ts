e871a6040f51319d9750f6fb6c85b90cb07f58e2
Alexandra marval <Alexandra.marval@eurokaizen.com>
2024-09-13 13:10:10 +0200

    feat: ✨ We get the types by the only value

--- src/app/useCases/home/components/profile/portfolio/portfolio.component.ts
+++ src/app/useCases/home/components/profile/portfolio/portfolio.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { ProfileService } from '../../../shared/services/profile/profile.service';
 import { AsyncPipe, NgClass, NgFor, NgIf } from '@angular/common';
 import { WebPage } from '../../../shared/models/webPage';
@@ -10,27 +10,28 @@ import { WebPage } from '../../../shared/models/webPage';
   templateUrl: './portfolio.component.html',
   styles: ``,
 })
-export class PortfolioComponent {
+export class PortfolioComponent implements OnInit {
   private profileService: ProfileService = inject(ProfileService);
   profile$ = this.profileService.profile$;
   webPage: WebPage[] = [];
-  
+
+  ngOnInit(): void {
+    this.profile$.subscribe((profile) => {
+      if (profile && profile.portfolio) {
+        this.webPage = profile.portfolio.webPage;
+        this.webPage = this.obtenerLosTipos();
+      }
+    });
+  }
+
   obtenerLosTipos(): WebPage[] {
     const unicos: string[] = [];
     return this.webPage.reduce((reduciendo, valor) => {
       if (!unicos.includes(valor.type)) {
         unicos.push(valor.type);
-        reduciendo.push({ type: valor.type,
-          position: 1, 
-          title: '',    
-          description: '', 
-          link: '',     
-          image: '' 
-         });
+        reduciendo.push({ type: valor.type, position: 1, title: '', description: '', link: '', image: '' });
       }
       return reduciendo;
     }, [] as WebPage[]);
   }
-
-  // types: string[] = ['CAT', 'NATURE', 'MARRIES'];
 }
