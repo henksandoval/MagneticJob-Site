e8e4c018a5f26df60e5617e2795740343dc5450e
Henk Sandoval <henkalexander.sandoval@external.wolterskluwer.com>
2024-09-13 12:27:31 +0200

    feat: ✨ Add Testimonials interface to template

--- src/app/useCases/home/components/profile/testimonials/testimonials.component.ts
+++ src/app/useCases/home/components/profile/testimonials/testimonials.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ProfileService } from '../../../shared/services/profile/profile.service';
+import { AsyncPipe, NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-testimonials',
   standalone: true,
-  imports: [],
+  imports: [NgIf, NgFor, AsyncPipe],
   templateUrl: './testimonials.component.html',
   styles: ``,
 })
-export class TestimonialsComponent {}
+export class TestimonialsComponent {
+  private profileService: ProfileService = inject(ProfileService);
+  profile$ = this.profileService.profile$;
+}
