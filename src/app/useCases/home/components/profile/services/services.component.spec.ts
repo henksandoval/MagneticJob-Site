eb41d5cda4367195c0992b162263c225dad02867
Alexandra marval <Alexandra.marval@eurokaizen.com>
2024-09-17 12:44:01 +0200

    test: ✅ It should show the title in the view

--- src/app/useCases/home/components/profile/services/services.component.spec.ts
+++ src/app/useCases/home/components/profile/services/services.component.spec.ts
@@ -1,15 +1,28 @@
-import { Service, ServiceDetails } from '../../../shared/models/service';
+import { of } from 'rxjs';
+import { mockProfile } from '../../../shared/tests/mocks/mockProfile';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { ProfileService } from '../../../shared/services/profile/profile.service';
+import { render, screen } from '@testing-library/angular';
+import '@testing-library/jest-dom';
+import { ServicesComponent } from './services.component';
 
-export const mockServiceDetails: ServiceDetails = {
-  name: 'SEO Consulting',
-  description:
-    "We optimize your website's performance and develop strategies to attract users interested in purchasing your products or hiring your services.",
-  backgroundUrl:
-    'https://www.bing.com/images/search?view=detailV2&ccid=KfOHRb47&id=0FD86363309638790692EF7C087E0578B8C18CB8&thid=OIP.KfOHRb47qEB8y3vbKpTifQHaEU&mediaurl=https%3a%2f%2fwww.campustraining.es%2fwp-content%2fuploads%2f2020%2f12%2ffunciones-fotografo-profesional.jpg&cdnurl=https%3a%2f%2fth.bing.com%2fth%2fid%2fR.29f38745be3ba8407ccb7bdb2a94e27d%3frik%3duIzBuHgFfgh87w%26pid%3dImgRaw%26r%3d0&exph=918&expw=1571&q=imagen+de+fotografo&simid=608028028367489008&FORM=IRPRST&ck=5264DEF23DC72764EC24610924AC8C9E&selectedIndex=18&itb=0',
+const mockProfileService = {
+  profile$: of(mockProfile),
+  loadProfile: jest.fn(),
 };
 
-export const mockService: Service = {
-    title: "Services",
-    overview: "We focus on understanding your company's needs to create a website tailored to your clients. You also have the support of our specialists in programming, design, and digital marketing to guide you through the process and resolve any questions you may have.",
-    serviceDetails: [mockServiceDetails]
-}
+const renderComponent = async () => {
+  await render(ServicesComponent, {
+    providers: [provideHttpClientTesting(), { provide: ProfileService, useValue: mockProfileService }],
+  });
+};
+
+describe('ServicesComponent', () => {
+    beforeEach(async () => {
+      await renderComponent();
+    });
+
+    it('It should show the title in the view', () => {
+        expect(screen.getByTestId('text')).toHaveTextContent(mockProfile.service.title);
+    });
+});
