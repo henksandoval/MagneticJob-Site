a31da3c70f1b8e8d44530e55a293f748c766d46d
Henk Sandoval <henk.sandoval@outlook.com>
2024-09-19 17:34:10 +0200

    feat: ✨ Config internazionalization by header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TranslateModule],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styles: ``,
 })
 export class AppComponent {
-  title = 'MySite';
-
   constructor(public translate: TranslateService) {
     translate.addLangs(['en', 'es']);
     translate.setDefaultLang('en');
