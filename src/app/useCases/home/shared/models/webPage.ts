6ce0328f9390321d6f82e65e92b77ab4e3832879
Henk Sandoval <henkalexander.sandoval@external.wolterskluwer.com>
2024-09-27 20:48:54 +0200

    feat: ✨ Implement GLightbox to project

--- src/app/useCases/home/shared/models/webPage.ts
+++ src/app/useCases/home/shared/models/webPage.ts
@@ -2,7 +2,8 @@ export interface WebPage {
   position: number;
   title: string;
   description: string;
-  link: string;
-  image: string;
+  link?: string;
+  image?: string;
+  video?: string;
   type: string;
 }
