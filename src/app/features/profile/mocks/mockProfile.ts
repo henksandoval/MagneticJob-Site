efd3113b63259de4c5680fc25994c7fbb9c14e31
Henk Sandoval <henk.sandoval@outlook.com>
2024-10-09 15:02:10 +0200

    chore: 🚚 Move summary mocks

--- src/app/features/profile/mocks/mockProfile.ts
+++ src/app/features/profile/mocks/mockProfile.ts
@@ -1,17 +1,15 @@
 import { Profile } from '../interfaces/profile';
 import { mockAbout } from '../components/about/mockAbout';
-import { mockTitles } from './mockTitles';
-import { mockSummary } from './mockSummary';
-import { mockEducation } from './mockEducation';
 import { mockWorkExperience } from './mockWorkExperience';
 import { mockSkills } from '../components/skills/mockSkills';
 import { mockPortfolio } from '../components/portfolio/mockPortfolio';
 import { mockTestimonials } from '../components/testimonials/mockTestimonials';
 import { mockService } from '../components/services/mockService';
+import { mockSummary } from '../components/summary/mockSummary';
+import { mockEducation } from '../components/summary/mockEducation';
 
 export const mockProfile: Profile = {
   about: mockAbout,
-  titles: mockTitles,
   summary: mockSummary,
   education: mockEducation,
   workExperience: mockWorkExperience,
