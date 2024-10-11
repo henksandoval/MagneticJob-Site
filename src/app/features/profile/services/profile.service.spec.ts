2d4cc65a59123074a4e0debc985982656dc6ee18
Henk Sandoval <henk.sandoval@outlook.com>
2024-10-11 13:07:17 +0200

    test: ✅ Recover Path use to unit tests

--- src/app/features/profile/services/profile.service.spec.ts
+++ src/app/features/profile/services/profile.service.spec.ts
@@ -1,6 +1,6 @@
 import { MockBuilder, MockRender, ngMocks } from 'ng-mocks';
 import { ProfileService } from './profile.service';
-import { HttpService } from '../../../core/services/http/http.service';
+import { HttpService } from '@core/services/http/http.service';
 
 describe('ProfileService', () => {
   beforeEach(() => MockBuilder(ProfileService).mock(HttpService));
