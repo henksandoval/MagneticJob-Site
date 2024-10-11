812002cc8dcb75d6a5e0ce598dfa1c1acef2688f
Henk Sandoval <henkalexander.sandoval@external.wolterskluwer.com>
2024-10-11 11:03:30 +0200

    chore: 💩 Try use HttpService in ProfileService instead of HttpClient of Angular

--- src/app/features/profile/services/profile.service.spec.ts
+++ src/app/features/profile/services/profile.service.spec.ts
@@ -1,24 +1,14 @@
-import { TestBed } from '@angular/core/testing';
-import { provideHttpClient } from '@angular/common/http';
-import { provideHttpClientTesting, HttpTestingController } from '@angular/common/http/testing';
+import { MockBuilder } from 'ng-mocks';
 import { ProfileService } from './profile.service';
-import { mockProfile } from '../mocks/profile.mock';
+import { of } from 'rxjs';
+import { HttpService } from '../../../core/services/http/http.service';
 
 describe('ProfileService', () => {
   let service: ProfileService;
-  let httpMock: HttpTestingController;
+  let httpMock: HttpService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [ProfileService, provideHttpClient(), provideHttpClientTesting()],
-    });
-
-    service = TestBed.inject(ProfileService);
-    httpMock = TestBed.inject(HttpTestingController);
-  });
-
-  afterEach(() => {
-    httpMock.verify();
+    MockBuilder(ProfileService).mock(HttpService);
   });
 
   it('should be created', () => {
@@ -26,37 +16,10 @@ describe('ProfileService', () => {
   });
 
   it('should load profile data successfully', () => {
-    service.loadProfile();
-
-    const testRequest = httpMock.expectOne('stubs/data.json');
-    expect(testRequest.request.method).toBe('GET');
-
-    testRequest.flush(mockProfile);
-
-    expect(service.profile$()).toEqual(mockProfile);
-    expect(service.profile$()).not.toBeNull();
-  });
-
-  it('should handle errors when loading profile data', () => {
-    service.loadProfile();
-
-    const testRequest = httpMock.expectOne('stubs/data.json');
-    expect(testRequest.request.method).toBe('GET');
-
-    testRequest.error(new ProgressEvent('Network error'));
-
-    expect(service.profile$()).toBeNull();
-  });
-
-  it('should not reload the profile if already loaded', () => {
-    service.loadProfile();
-
-    const testRequest = httpMock.expectOne('stubs/data.json');
-    testRequest.flush(mockProfile);
+    spyOn(httpMock, 'get').and.returnValue(of({}));
 
     service.loadProfile();
 
-    httpMock.expectNone('assets/data.json');
-    expect(service.profile$()).toEqual(mockProfile);
+    expect(httpMock.get).toHaveBeenCalledWith('stubs/data.json');
   });
 });
