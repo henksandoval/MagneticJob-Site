e5bae3ac0605994637f03c49ad8db9b4a08d7226
Henk Sandoval <henk.sandoval@outlook.com>
2024-10-11 08:02:45 +0200

    chore: ✅ Add HTTP service abstraction with GET, POST, PUT, DELETE and PATCH

---
+++ src/app/core/services/http/http.service.ts
@@ -0,0 +1,30 @@
+import { inject, Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HttpService {
+  private readonly http = inject(HttpClient);
+
+  get<TOut>(url: string, params?: HttpParams): Observable<TOut> {
+    return this.http.get<TOut>(url, { params });
+  }
+
+  post<TIn, TOut>(url: string, body: TIn, options?: { headers?: HttpHeaders }): Observable<TOut> {
+    return this.http.post<TOut>(url, body, options);
+  }
+
+  put<TIn, TOut>(url: string, body: TIn, options?: { headers?: HttpHeaders }): Observable<TOut> {
+    return this.http.put<TOut>(url, body, options);
+  }
+
+  patch<TIn, TOut>(url: string, body: TIn, options?: { headers?: HttpHeaders }): Observable<TOut> {
+    return this.http.patch<TOut>(url, body, options);
+  }
+
+  delete<TOut>(url: string, options?: { headers?: HttpHeaders }): Observable<TOut> {
+    return this.http.delete<TOut>(url, options);
+  }
+}
