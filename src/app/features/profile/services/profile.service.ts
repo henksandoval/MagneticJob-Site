07c3dce8ac0b01d59fbc46cc56458c8710dca674
Dany Paredes <danywalls4@gmail.com>
2024-10-13 16:01:39 +0200

    fix(profile): remove suscription to observable

--- src/app/features/profile/services/profile.service.ts
+++ src/app/features/profile/services/profile.service.ts
@@ -1,32 +1,22 @@
-import { computed, inject, Injectable, signal } from '@angular/core';
-import { catchError, of, tap } from 'rxjs';
+import { inject, Injectable } from '@angular/core';
+import { catchError, EMPTY, Observable } from 'rxjs';
 import { Profile } from '../interfaces/profile';
 import { HttpService } from '@core/services/http/http.service';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProfileService {
   private readonly http = inject(HttpService);
-  private readonly profileSource = signal<Profile | null>(null);
-  private readonly profileLoaded = signal(false);
 
-  profile$ = computed(() => this.profileSource());
+  profile$ = toSignal(this.loadProfile());
 
-  loadProfile(): void {
-    if (!this.profileLoaded()) {
-      this.http
-        .get<Profile>('stubs/data.json')
-        .pipe(
-          tap((data: Profile) => {
-            this.profileSource.set(data);
-            this.profileLoaded.set(true);
-          }),
-          catchError(() => {
-            return of(null);
-          })
-        )
-        .subscribe();
-    }
+  loadProfile(): Observable<Profile> {
+    return this.http.get<Profile>('stubs/data.json').pipe(
+      catchError(() => {
+        return EMPTY;
+      })
+    );
   }
 }
