dd2df42c423741c2013431451b99cb1834e03f6a
Henk Sandoval <henkalexander.sandoval@external.wolterskluwer.com>
2024-10-23 20:49:41 +0200

    refactor: ♻️ Use bucle for by load sections of menu

--- src/app/shared/layout/header/header.component.ts
+++ src/app/shared/layout/header/header.component.ts
@@ -1,19 +1,18 @@
 import { Component, inject, model } from '@angular/core';
 import { PageScrollService } from 'ngx-page-scroll-core';
-import { DOCUMENT, JsonPipe, NgClass, NgFor } from '@angular/common';
-import { DEFAULT_SECTION, MenuSection } from './section';
+import { DOCUMENT, NgClass, NgFor } from '@angular/common';
+import { MenuSection } from './section';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [NgClass, NgFor, JsonPipe],
+  imports: [NgClass, NgFor],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
   sections = model<Map<string, MenuSection>>();
   activeSectionId = 'hero';
-  defaultSection = DEFAULT_SECTION;
 
   private readonly pageScrollService: PageScrollService = inject(PageScrollService);
   private readonly document: Document = inject(DOCUMENT);
