63ca10ff5effb4170b333f7464aaeac644037378
Henk Sandoval <henk.sandoval@outlook.com>
2024-10-23 05:43:20 +0200

    chore: 💩 Implement deferable load first approach

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,14 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { HeaderComponent } from '../../shared/layout/header/header.component';
 import { CoverComponent } from '../../shared/layout/cover/cover.component';
 import { FooterComponent } from '../../shared/layout/footer/footer.component';
 import { ProfileComponent } from '../profile/profile.component';
+import { JsonPipe } from '@angular/common';
 
+// eslint-disable-next-line @typescript-eslint/consistent-type-definitions
+export type menuSection = {
+  key: string;
+  icon: string;
+  label: string;
+  isActive: boolean;
+};
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, CoverComponent, FooterComponent, ProfileComponent],
+  imports: [HeaderComponent, CoverComponent, FooterComponent, ProfileComponent, JsonPipe],
   templateUrl: './home.component.html',
   styles: ``,
 })
-export class HomeComponent {}
+export class HomeComponent {
+  private _menuSections = {
+    hero: { target: 'hero', icon: 'bx-home', label: 'Home', isActive: true },
+    about: { target: 'about', icon: 'bx-user', label: 'About', isActive: false },
+    resume: { target: 'resume', icon: 'bx-file-blank', label: 'Resume', isActive: false },
+    portfolio: { target: 'portfolio', icon: 'bx-book-content', label: 'Portfolio', isActive: false },
+    skills: { target: 'skills', icon: 'bx-brain', label: 'Skills', isActive: false },
+    facts: { target: 'facts', icon: 'bx-user', label: 'Facts', isActive: false },
+    services: { target: 'services', icon: 'bx-server', label: 'Services', isActive: false },
+    testimonials: { target: 'testimonials', icon: 'bx-server', label: 'Testimonials', isActive: false },
+    contact: { target: 'contact', icon: 'bx-envelope', label: 'Contact', isActive: false },
+  };
+
+  sections = signal(this._menuSections);
+}
