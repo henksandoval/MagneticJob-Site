7b016ffc49b679eaa4706f1a979b6d9bad05bf20
Henk Sandoval <henk.sandoval@outlook.com>
2024-10-23 07:02:45 +0200

    refactor: ♻️ Add safe type to MenuSections using Map

--- src/app/shared/layout/header/header.component.ts
+++ src/app/shared/layout/header/header.component.ts
@@ -1,7 +1,7 @@
 import { Component, inject, model } from '@angular/core';
 import { PageScrollService } from 'ngx-page-scroll-core';
 import { DOCUMENT, JsonPipe, NgClass, NgFor } from '@angular/common';
-import { MenuSection } from './section';
+import { DEFAULT_SECTION, MenuSection } from './section';
 
 @Component({
   selector: 'app-header',
@@ -11,9 +11,9 @@ import { MenuSection } from './section';
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  sections = model<any>();
+  sections = model<Map<string, MenuSection>>();
   activeSectionId = 'hero';
+  defaultSection = DEFAULT_SECTION;
 
   private readonly pageScrollService: PageScrollService = inject(PageScrollService);
   private readonly document: Document = inject(DOCUMENT);
--- src/app/shared/layout/header/section.ts
+++ src/app/shared/layout/header/section.ts
@@ -1,16 +1,21 @@
 export interface MenuSection {
-  key: string;
-  icon: string;
-  label: string;
+  target?: string;
+  icon?: string;
+  label?: string;
   isActive: boolean;
 }
+export const DEFAULT_SECTION: MenuSection = {
+  isActive: false,
+};
 
-export const SECTIONS: MenuSection[] = [
-  { key: 'hero', icon: 'bx-home', label: 'Home', isActive: true },
-  { key: 'about', icon: 'bx-user', label: 'About', isActive: false },
-  { key: 'resume', icon: 'bx-file-blank', label: 'Resume', isActive: false },
-  { key: 'portfolio', icon: 'bx-book-content', label: 'Portfolio', isActive: false },
-  { key: 'skills', icon: 'bx-brain', label: 'Skills', isActive: false },
-  { key: 'services', icon: 'bx-server', label: 'Services', isActive: false },
-  { key: 'contact', icon: 'bx-envelope', label: 'Contact', isActive: false },
-];
+export const MENU_SECTIONS = new Map<string, MenuSection>([
+  ['hero', { target: 'hero', icon: 'bx-home', label: 'Home', isActive: true }],
+  ['about', { target: 'about', icon: 'bx-user', label: 'About', isActive: false }],
+  ['resume', { target: 'resume', icon: 'bx-file-blank', label: 'Resume', isActive: false }],
+  ['portfolio', { target: 'portfolio', icon: 'bx-book-content', label: 'Portfolio', isActive: false }],
+  ['skills', { target: 'skills', icon: 'bx-brain', label: 'Skills', isActive: false }],
+  ['facts', { target: 'facts', icon: 'bx-user', label: 'Facts', isActive: false }],
+  ['services', { target: 'services', icon: 'bx-server', label: 'Services', isActive: false }],
+  ['testimonials', { target: 'testimonials', icon: 'bx-server', label: 'Testimonials', isActive: false }],
+  ['contact', { target: 'contact', icon: 'bx-envelope', label: 'Contact', isActive: false }],
+]);
