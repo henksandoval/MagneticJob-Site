a37c95c978096426c4b79277f421398eab3c499b
Henk Sandoval <henk.sandoval@outlook.com>
2024-10-27 17:11:45 +0100

    chore: 💩 Add behavior by write profession in cover

--- src/app/shared/layouts/cover/cover.component.ts
+++ src/app/shared/layouts/cover/cover.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cover',
@@ -7,4 +7,38 @@ import { Component } from '@angular/core';
   templateUrl: './cover.component.html',
   styleUrl: './cover.component.scss',
 })
-export class CoverComponent {}
+export class CoverComponent implements OnInit, OnDestroy {
+  professions: string[] = ['Designer', 'Developer', 'Freelancer', 'Photographer'];
+  currentProfessionIndex = 0;
+  currentProfession = '';
+  intervalId = 0;
+
+  ngOnInit() {
+    this.typeProfession();
+  }
+
+  ngOnDestroy() {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
+
+  typeProfession() {
+    let charIndex = 0;
+    const type = () => {
+      if (charIndex < this.professions[this.currentProfessionIndex].length) {
+        this.currentProfession += this.professions[this.currentProfessionIndex][charIndex];
+        charIndex++;
+        setTimeout(type, 100);
+      } else {
+        setTimeout(() => {
+          this.currentProfession = '';
+          this.currentProfessionIndex = (this.currentProfessionIndex + 1) % this.professions.length;
+          charIndex = 0;
+          setTimeout(type, 500);
+        }, 1000);
+      }
+    };
+    type();
+  }
+}
