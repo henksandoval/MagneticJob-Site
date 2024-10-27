bc3a41b55c693c5d55a35434598e481d70bb264a
Henk Sandoval <henk.sandoval@outlook.com>
2024-10-27 18:01:51 +0100

    refactor: 🚚 Change structure folder
    Define a new feature call MyResume

---
+++ src/app/features/MyResume/home/components/profile/profile.component.ts
@@ -0,0 +1,38 @@
+import { Component, inject, model } from '@angular/core';
+import { HeaderComponent } from '../../../layouts/header/header.component';
+import { CoverComponent } from '../cover/cover.component';
+import { AboutComponent } from './components/about/about.component';
+import { FactComponent } from './components/fact/fact.component';
+import { SkillsComponent } from './components/skils/skills.component';
+import { SummaryComponent } from './components/summary/summary.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { ServicesComponent } from './components/services/services.component';
+import { TestimonialsComponent } from './components/testimonials/testimonials.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ProfileService } from './services/profile.service';
+import { NgIf } from '@angular/common';
+import { MenuSection } from '../../../layouts/header/interfaces/menu-section';
+
+@Component({
+  selector: 'app-profile',
+  standalone: true,
+  imports: [
+    NgIf,
+    HeaderComponent,
+    CoverComponent,
+    AboutComponent,
+    FactComponent,
+    SkillsComponent,
+    SummaryComponent,
+    PortfolioComponent,
+    ServicesComponent,
+    TestimonialsComponent,
+    ContactComponent,
+  ],
+  templateUrl: './profile.component.html',
+})
+export class ProfileComponent {
+  private readonly profileService: ProfileService = inject(ProfileService);
+  profile$ = this.profileService.profile$;
+  sections = model<Map<string, MenuSection>>();
+}
