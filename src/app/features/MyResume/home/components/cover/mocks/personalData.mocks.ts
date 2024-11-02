61975acf26be14a8158da9cfe42ee0768507a49b
Henk Sandoval <henkalexander.sandoval@external.wolterskluwer.com>
2024-11-02 21:41:21 +0100

    refactor: ♻️ Refactor profile service by use social network types

---
+++ src/app/features/MyResume/home/components/cover/mocks/personalData.mocks.ts
@@ -0,0 +1,12 @@
+﻿import { UserPersonalData } from '../interfaces/user-personal-data';
+import { SocialNetworkTypes } from '@core/Constants/social-network-def';
+
+export const mockPersonalData: UserPersonalData = {
+  name: 'Test Name',
+  professions: ['Developer', 'QA', 'DevOps'],
+  socialNetworks: [
+    { type: SocialNetworkTypes.Facebook, url: 'http://facebook.com/userprofile' },
+    { type: SocialNetworkTypes.Twitter, url: 'http://twitter.com/userprofile' },
+    { type: SocialNetworkTypes.LinkedIn, url: 'http://linkedin.com/userprofile' },
+  ],
+};
--- src/app/features/MyResume/home/services/profile.service.ts
+++ src/app/features/MyResume/home/services/profile.service.ts
@@ -1,9 +1,14 @@
 import { inject, Injectable } from '@angular/core';
-import { catchError, EMPTY, Observable } from 'rxjs';
+import { catchError, EMPTY, map, Observable } from 'rxjs';
 import { Profile } from '../components/profile/interfaces/profile';
+import { ProfileContract } from '../components/profile/contracts/profile-contract';
 import { HttpService } from '@core/services/http/http.service';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { StateService } from '@core/services/state/state.service';
+import { UserPersonalDataContract } from '../components/cover/contracts/user-personal-data-contract';
+import { UserPersonalData } from '../components/cover/interfaces/user-personal-data';
+import { SocialNetworkTypes } from '@core/Constants/social-network-def';
+import { UserSocialNetwork } from '../components/cover/interfaces/user-social-network';
 
 @Injectable({
   providedIn: 'root',
@@ -16,10 +21,49 @@ export class ProfileService {
 
   private loadProfile(userName: string): Observable<Profile> {
     const url = `stubs/data.${userName || 'john'}.json`;
-    return this.http.get<Profile>(url).pipe(
-      catchError(() => {
+    return this.http.get<ProfileContract>(url).pipe(
+      map(this.transformData.bind(this)),
+      catchError((error) => {
+        console.error(error);
         return EMPTY;
       })
     );
+  }
+
+  transformData(data: ProfileContract): Profile {
+    const personalData: UserPersonalData = this.transformPersonaData(data.personalData);
+    const contract: Profile = {
+      personalData: personalData,
+      about: data.about,
+      service: data.service,
+      birthday: data.birthday,
+      contact: data.contact,
+      facts: data.facts,
+      portfolio: data.portfolio,
+      skillSet: data.skillSet,
+      summary: data.summary,
+      testimonials: data.testimonials,
+    };
+    return contract;
+  }
+
+  transformPersonaData(data: UserPersonalDataContract): UserPersonalData {
+    const personalData: UserPersonalData = {
+      name: data.name,
+      professions: data.professions,
+      socialNetworks: data.socialNetworks.reduce((accumulator, network) => {
+        const validNetwork = SocialNetworkTypes[network.name as keyof typeof SocialNetworkTypes];
+        if (validNetwork) {
+          accumulator.push({
+            type: validNetwork,
+            url: network.url,
+          });
+        } else {
+          console.warn(`Unexpected social network name: ${network.name}`);
+        }
+        return accumulator;
+      }, [] as UserSocialNetwork[]),
+    };
+    return personalData;
   }
 }
