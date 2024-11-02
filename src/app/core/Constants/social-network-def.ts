960e99a472a15df59c5162c012b540e50b59dc29
Henk Sandoval <henkalexander.sandoval@external.wolterskluwer.com>
2024-11-02 21:39:54 +0100

    refactor: ♻️ Use data-id directive in all components

---
+++ src/app/core/Constants/social-network-def.ts
@@ -0,0 +1,28 @@
+﻿import { SocialNetworkInfo } from '@core/interfaces/social-network-info';
+
+type SocialNetworkType = 'Facebook' | 'Twitter' | 'LinkedIn' | 'Instagram' | 'Skype';
+
+type SocialNetworkDef = Record<SocialNetworkType, SocialNetworkInfo>;
+
+export const SocialNetworkTypes: SocialNetworkDef = {
+  Facebook: {
+    name: 'Facebook',
+    icon: 'bxl-facebook',
+  },
+  Twitter: {
+    name: 'Twitter',
+    icon: 'bxl-twitter',
+  },
+  Instagram: {
+    name: 'Instagram',
+    icon: 'bxl-instagram',
+  },
+  LinkedIn: {
+    name: 'LinkedIn',
+    icon: 'bxl-linkedin',
+  },
+  Skype: {
+    name: 'Skype',
+    icon: 'bxl-skype',
+  },
+} as const;
--- src/app/features/MyResume/home/components/profile/components/about/about.component.ts
+++ src/app/features/MyResume/home/components/profile/components/about/about.component.ts
@@ -1,13 +1,14 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common';
 import { About } from './interfaces/about';
+import { AppIdDirective } from '@core/directives/app-id/app-id.directive';
 
 @Component({
   selector: 'app-about',
   standalone: true,
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss',
-  imports: [NgOptimizedImage],
+  imports: [NgOptimizedImage, AppIdDirective],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AboutComponent {
