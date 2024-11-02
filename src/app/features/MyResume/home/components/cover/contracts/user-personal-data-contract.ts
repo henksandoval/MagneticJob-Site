5ff06994058f08b29e4481d4f3a394f227cdfd33
Henk Sandoval <henk.sandoval@outlook.com>
2024-11-02 21:41:21 +0100

    refactor: ♻️ Refactor profile service by use social network types

---
+++ src/app/features/MyResume/home/components/cover/contracts/user-personal-data-contract.ts
@@ -0,0 +1,7 @@
+﻿import { UserSocialNetworkContract } from './user-social-network-contract';
+
+export interface UserPersonalDataContract {
+  name: string;
+  professions: string[];
+  socialNetworks: UserSocialNetworkContract[];
+}
