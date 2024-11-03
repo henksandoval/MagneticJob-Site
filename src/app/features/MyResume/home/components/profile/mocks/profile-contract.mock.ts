e4ce6a3db296e1e895f2caae079547423b8e8e60
Henk Sandoval <henkalexander.sandoval@external.wolterskluwer.com>
2024-11-03 09:37:31 +0100

    test: 🧪 Adjusts profile service unit tests

---
+++ src/app/features/MyResume/home/components/profile/mocks/profile-contract.mock.ts
@@ -0,0 +1,28 @@
+﻿import { ProfileContract } from '../contracts/profile-contract';
+import { mockSummary } from '../components/summary/mocks/summary.mock';
+import { mockAbout } from '../components/about/mocks/about.mock';
+import { mockPortfolio } from '../components/portfolio/mocks/portfolio.mock';
+import { mockService } from '../components/services/mocks/service.mock';
+import { mockSkills } from '../components/skils/mocks/skills.mock';
+import { mockTestimonials } from '../components/testimonials/mocks/testimonials.mocks';
+import { mockContact } from '../components/contact/mocks/contactProfile.mock';
+import { mockPersonalDataContract } from '../../cover/mocks/personal-data-contract.mocks';
+
+export const mockProfileContract: ProfileContract = {
+  personalData: mockPersonalDataContract,
+  birthday: '01/01/1990',
+  contact: mockContact,
+  about: mockAbout,
+  facts: {
+    Awards: 'awards',
+    description: 'description',
+    happyClients: 'happy',
+    hoursOfSupport: '10',
+    projects: 'project',
+  },
+  skillSet: mockSkills,
+  summary: mockSummary,
+  testimonials: mockTestimonials,
+  portfolio: mockPortfolio,
+  service: mockService,
+};
